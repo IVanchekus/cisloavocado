0b97509b001d6d8892df029087ca79cd4b22f9e6
IVanchekus <ivan.gravdan@mail.ru>
2025-01-02 18:49:59 +0700

    clear and other

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,9 @@
 <template>
+  <Navbar />
   <router-view></router-view>
 </template>
+
+<script setup>
+import Navbar from "./components/Navbar.vue";
+
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li><a>Главная</a></li>
+      <li><a>Математика</a></li>
+      <li><a>Физика</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.navbar {
+  height: 100px;
+  background-color: #1E1E1E;
+  color: white;
+}
+</style>

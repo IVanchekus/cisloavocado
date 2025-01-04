f4cc0ee8951d1ac5afa47a737bdb0961e461fa81
IVanchekus <ivan.gravdan@mail.ru>
2025-01-04 20:35:54 +0700

    navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="card">
-    <Menubar :model="menuItems">
+    <Menubar :model="menuItems" style="height: 100px;">
       <template #start>
         <Logo></Logo>
+      </template>
+      <template #item="{ item }">
+        {{ item.label }}
+      </template>
+      <template #end>
+        <router-link to="/login">
+          <Button label="Войти" />
+        </router-link>
       </template>
     </Menubar>
   </div>
@@ -10,6 +18,7 @@
 
 <script setup>
 import Menubar from 'primevue/menubar';
+import { Button } from 'primevue';
 import { ref } from 'vue';
 import Logo from "@/assets/logo.svg";
 

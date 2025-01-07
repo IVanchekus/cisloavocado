8cf86a769ac27c16799f5134ac6aadd757108579
IVanchekus <ivan.gravdan@mail.ru>
2025-01-08 00:29:56 +0700

    page template

---
+++ frontend/src/pages/PageTemplate/PageTemplate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-template">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { defineSlots } from 'vue';
+import { ISlots } from './PageTemplate.types';
+
+const slots = defineSlots<ISlots>();
+</script>
+
+<style scoped>
+.page-template {
+  height: 100px;
+  padding: 19px 11.5%;
+}
+
+@media (max-width: 1248px) {
+  .page-template {
+    padding: 19px 15px;
+  }
+}
+</style>
